61e9716542eabe26709396dcf61228e532bb505a
alberto ruano <marianagonzalezlopez01@gmail.com>
2020-03-04 20:54:16 -0600

    fileUpload added in Index View

--- ecommerceED1_2/ecommerceED1_2/Controllers/HomeController.cs
+++ ecommerceED1_2/ecommerceED1_2/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+
 
 namespace ecommerceED1_2.Controllers
 {
@@ -10,6 +12,20 @@ namespace ecommerceED1_2.Controllers
     {
         public ActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+
+            var fileName = string.Empty;
+            var path = string.Empty;
+            if (file.ContentLength > 0)
+            {
+                fileName = Path.GetFileName(file.FileName);
+                path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                file.SaveAs(path);
+            }
             return View();
         }
 
